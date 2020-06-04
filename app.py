30dcbb6ea85df8361971f9e93cce09d48081d356
peteruche21 <anyaogupeter601@gmail.com>
2020-06-04 18:46:23 +0100

    portfolio

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('peso.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    if page_name == 'thankyou.html':
+        return render_template('page_not_found.html'), 404
+    else:
+        return render_template(page_name)
+
+def writ_tofile(data):
+    with open('db.csv', mode='a') as db:
+        email = data['email']
+        name = data['name']
+        message = data['message']
+        csv_write = csv.writer(db, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_write.writerow([name, email, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            name = request.form['name']
+            writ_tofile(data)
+            return redirect(url_for('thankyou', name=name))
+        except:
+            return 'did not write to database'
+    else:
+        return 'something went wrong, try again'
+
+@app.route('/thankyou')
+def thankyou():
+    name = request.args.get('name', None)
+    if name == None:
+        return render_template('page_not_found.html'), 404
+    elif name.isnumeric():
+        return 'something went wrong, check your details and try again'
+    else:
+        return render_template('thankyou.html', name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

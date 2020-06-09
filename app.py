ebf88efe5d2b1cfcb06b59342fac7c2b85fdbb98
peteruche21 <anyaogupeter601@gmail.com>
2020-06-09 20:23:33 +0100

    updated 404 error code

--- app.py
+++ app.py
@@ -4,18 +4,25 @@ import csv
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('page_not_found.html'), 404
+
+
 @app.route('/')
 def home():
     return render_template('peso.html')
 
+
 @app.route('/<string:page_name>')
 def html_page(page_name):
     if page_name == 'thankyou.html':
-        return render_template('page_not_found.html'), 404
+        return page_not_found()
     else:
         return render_template(page_name)
 
-def writ_tofile(data):
+
+def writ_to_file(data):
     with open('db.csv', mode='a') as db:
         email = data['email']
         name = data['name']
@@ -30,23 +37,24 @@ def submit_form():
         try:
             data = request.form.to_dict()
             name = request.form['name']
-            writ_tofile(data)
+            writ_to_file(data)
             return redirect(url_for('thankyou', name=name))
         except:
             return 'did not write to database'
     else:
         return 'something went wrong, try again'
 
+
 @app.route('/thankyou')
 def thankyou():
     name = request.args.get('name', None)
-    if name == None:
-        return render_template('page_not_found.html'), 404
+    if name is None:
+        return page_not_found()
     elif name.isnumeric():
         return 'something went wrong, check your details and try again'
     else:
         return render_template('thankyou.html', name=name)
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
